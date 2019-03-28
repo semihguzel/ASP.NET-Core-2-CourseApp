6fe0f19e4ee2d14a09582371d1dd547bb6d16ef4
Semih Güzel <semih_guzel@outlook.com>
2019-03-28 19:06:32 +0300

    Action yönlendirmeleri, bir action içerisinden farklı view'lara yönlendirmeler, static bir liste içerisinde repository ile öğrenci ekleme işlemleri yapıldı.

--- CourseApp.Web/Controllers/CourseController.cs
+++ CourseApp.Web/Controllers/CourseController.cs
@@ -2,21 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CourseApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CourseApp.Web.Controllers
 {
     public class CourseController : Controller
     {
-        public string Index()
+        //Course/MyView
+        public ViewResult Index()
         {
-
-            return "course/index";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour > 12 ? "Good Afternoon" : "Good Morning";
+            return View("MyView");
+        }
+        //Course/List
+        public ViewResult List()
+        {
+            return View();
         }
 
-        public string List()
+        public ViewResult Apply()
         {
-            return "course/list";
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Apply(Student student)
+        {
+            //ASP.NET MVC yapısında FormCollection kullanarak Html.Helper ile yolladığımız form nesnesinden istediğimiz verileri alıp entity'mize göre eşitleme işlemi yapıyorduk. .Net Core 2'de ise TagHelper ile form yapısında asp-for içerisine Model olarak yolladığımız property'lerimiz için oluşması gereken inputlar otomatik oluşuyor, bu verileri POST etmek istediğimizde ise HttpPost metodumuzda FormCollection nesnesi parametre olarak almak yerine, Model Binding yapısı sayesinde View üzerinden yollanan entity tipinde parametre vererek entity nesnesini direkt olarak alabiliyoruz.
+            //student entity'sine ait property verileri veri tabanına kayıt edilebilir.
+
+            Repository.AddStudent(student);
+            return View("Thanks",student);
         }
     }
 }
