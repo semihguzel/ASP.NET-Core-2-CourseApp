47550aa90d8b5084f72219726d704d31bd984d3c
Semih Güzel <semih_guzel@outlook.com>
2019-03-28 17:46:34 +0300

    Add project files.

---
+++ CourseApp.Web/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApp.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        public string Index()
+        {
+
+            return "course/index";
+        }
+
+        public string List()
+        {
+            return "course/list";
+        }
+    }
+}
---
+++ CourseApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var course = new Course()
+            {
+                Id = 1,
+                Name = "ASP.NET Core kursu"
+            };
+            return View(course);
+        }
+    }
+}
